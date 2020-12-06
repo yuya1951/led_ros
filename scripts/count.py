74cb3a772c2919d9952563a4f8b6d6e00d125f7d
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-06 11:49:16 +0900

    Update and rename ledpub.py to count.py

--- scripts/count.py
+++ scripts/count.py
@@ -6,8 +6,8 @@ from std_msgs.msg import Int32
 
 
 if __name__ == "__main__":
-	rospy.init_node("ledpub")
-	pub = rospy.Publisher("led", Int32, queue_size=1)
+	rospy.init_node("count")
+	pub = rospy.Publisher("count_up", Int32, queue_size=1)
         rate = rospy.Rate(10)
 	n = 0
         while not rospy.is_shutdown():
