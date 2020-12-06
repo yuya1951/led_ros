b3d31022608bbfd662dd17771da5df5c435d43b9
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-06 12:09:32 +0900

    Update ledpub.py

--- scripts/ledpub.py
+++ scripts/ledpub.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 	pub = rospy.Publisher("led", Int32, queue_size=1)
         rate = rospy.Rate(10)
 
-  while not rospy.is_shutdown():
-  n = input()
+ 	while not rospy.is_shutdown():
+  	n = input()
 	pub.publish(n)
-  rate.sleep()
+  	rate.sleep()
