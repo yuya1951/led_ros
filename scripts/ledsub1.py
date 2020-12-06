cc82ba38f432af122d9be5cf4d6f6d8c9eae345c
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-06 11:28:24 +0900

    Create ledsub1.py

---
+++ scripts/ledsub1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int32
+
+def cb(message):
+    n = message.data%16
+    rospy.loginfo(n)
+
+    if n == 0 :
+        with open("/dev/myled0", "w") as f:
+            f.write("0\n")
+
+    elif n == 1 :
+        with open("/dev/myled0", "w") as f:
+            f.write("1\n")
+
+    elif n == 2 :
+        with open("/dev/myled0", "w") as f:
+            f.write("2\n")
+
+    elif n == 3 :
+        with open("/dev/myled0", "w") as f:
+            f.write("3\n")
+
+    elif n == 4 :
+        with open("/dev/myled0", "w") as f:
+            f.write("4\n")
+
+    elif n == 5 :
+        with open("/dev/myled0", "w") as f:
+            f.write("5\n")
+
+    elif n == 6 :
+        with open("/dev/myled0", "w") as f:
+            f.write("6\n")
+
+    elif n == 7 :
+        with open("/dev/myled0", "w") as f:
+            f.write("7\n")
+
+    elif n == 8 :
+        with open("/dev/myled0", "w") as f:
+            f.write("8\n")
+
+    elif n == 9 :
+        with open("/dev/myled0", "w") as f:
+            f.write("9\n")
+
+
+if __name__ == '__main__':
+    rospy.init_node('twice')
+    sub = rospy.Subscriber('count_up', Int32, cb)
+    rospy.spin()
