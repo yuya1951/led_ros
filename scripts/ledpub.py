0978b198c9ab31500e2b7fe8ee088e83242c375d
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-06 11:54:39 +0900

    Create ledpub.py

---
+++ scripts/ledpub.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int32
+
+
+
+if __name__ == "__main__":
+	rospy.init_node("ledpub")
+	pub = rospy.Publisher("led", Int32, queue_size=1)
+        rate = rospy.Rate(10)
+
+  while not rospy.is_shutdown():
+  n = input()
+	pub.publish(n)
+  rate.sleep()
