ffc17ab03dc63e239c68cfb66498a7b319cb6c1a
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-09 14:33:58 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -8,7 +8,7 @@ from std_msgs.msg import Int32
 if __name__ == "__main__":
 	rospy.init_node("count")
 	pub = rospy.Publisher("count_up", Int32, queue_size=1)
-        rate = rospy.Rate(10)
+	rate = rospy.Rate(10)
 	n = 0
         while not rospy.is_shutdown():
             n += 1
