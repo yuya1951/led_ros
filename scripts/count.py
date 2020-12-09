da1c91499dd3529c95d0d76e4960239440c77d7e
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-09 14:39:45 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 	pub = rospy.Publisher("count_up", Int32, queue_size=1)
 	rate = rospy.Rate(10)
 	n = 0
-        while not rospy.is_shutdown():
+while not rospy.is_shutdown():
             n += 1
 	    pub.publish(n)
             rate.sleep()
