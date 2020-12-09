b77a50eab50c1297cb70d8d018d6f46f79f57fef
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-09 14:34:27 +0900

    Update ledpub.py

--- scripts/ledpub.py
+++ scripts/ledpub.py
@@ -8,7 +8,7 @@ from std_msgs.msg import Int32
 if __name__ == "__main__":
 	rospy.init_node("ledpub")
 	pub = rospy.Publisher("led", Int32, queue_size=1)
-        rate = rospy.Rate(10)
+	rate = rospy.Rate(10)
 
  	while not rospy.is_shutdown():
   		n = input()
