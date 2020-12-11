67cbcf74bd253e4d1abe6431e70609b2d47c325d
yuya1951 <71247013+yuya1951@users.noreply.github.com>
2020-12-11 13:39:42 +0900

    Update ledsub.py

--- scripts/ledsub.py
+++ scripts/ledsub.py
@@ -3,58 +3,46 @@
 import rospy
 from std_msgs.msg import Int32
 
-def callback(message):
+n = 0
 
-    if message == a:
-        with open("/dev/myled0", "w") as f:
-		f.write("0\n" )
-
-    if message == b :
-        with open("/dev/myled0", "w") as f:
-		f.write("1\n" )
-
-    if message == c:
-        with open("/dev/myled0", "w") as f:
-		f.write("2\n" )
+def cb(message):
+	global n
+	n = message.data
 	
-    if message == d:
-        with open("/dev/myled0", "w") as f:
-		f.write("3\n" )
-
-    if message == e :
-        with open("/dev/myled0", "w") as f:
-		f.write("4\n" )
-
-    if message == g:
-        with open("/dev/myled0", "w") as f:
-		f.write("5\n" )
+	if message == "0":
+		with open("/dev/myled0", "w") as f:
+			f.write("0\n" )
 	
-    if message == h:
-        with open("/dev/myled0", "w") as f:
-		f.write("6\n" )
+	if message == "1":
+		with open("/dev/myled0", "w") as f:
+			f.write("1\n" )
 
-    if message == i :
-        with open("/dev/myled0", "w") as f:
-		f.write("7\n" )
+	if message == "2":
+		with open("/dev/myled0", "w") as f:
+			f.write("2\n" )
+	
+	if message == "3":
+		with open("/dev/myled0", "w") as f:
+			f.write("3\n" )
 
-    
-    
+	if message == "4":
+		with open("/dev/myled0", "w") as f:
+			f.write("4\n" )
+
+	if message == "5":
+		with open("/dev/myled0", "w") as f:
+			f.write("5\n" )
 	
-	
-	
+	if message == "6":
+		with open("/dev/myled0", "w") as f:
+			f.write("6\n" )
+			
+	if message == "7":
+		with open("/dev/myled0", "w") as f:
+			f.write("7\n" )
+
+
 if __name__ == "__main__":
-	a = Int32(0)
-	b = Int32(1)
-	c = Int32(2)
-	d = Int32(3)
-	e = Int32(4)
-	g = Int32(5)
-	h = Int32(6)
-	i = Int32(7)
-	j = Int32(8)
-	
-	
-        rospy.init_node("ledsub")
+	rospy.init_node("ledsub")
 	sub = rospy.Subscriber("led", Int32, callback)
-	pub = rospy.Publisher("ledsub", Int32, queue_size = 1)
 	rospy.spin()
